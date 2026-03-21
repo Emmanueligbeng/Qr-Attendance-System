eb16c216f51d9fbb612559679f6267a1bac7c7fc
Emmanuel Igbeng <emmanueligbeng01@gmail.com>
2026-03-21 23:00:33 +0100

    updated requirements.txt file

--- qr_attendance_backend/venv/bin/activate.fish
+++ qr_attendance_backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/emmanuel-igbeng/Desktop/projects/qr_attendance_backend/venv
+set -gx VIRTUAL_ENV /home/emmanuel-igbeng/Desktop/attendance/qr_attendance_backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
